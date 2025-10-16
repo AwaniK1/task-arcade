a5080301f700fa10afdffba6032c6c39c555d30d
Awani K <awani.18159@sakec.ac.in>
2025-10-16 12:24:30 +0530

    Backend setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://awani18159_db_user:ThisMongo%402025@cluster0.c3mkj17.mongodb.net/taskarcade?retryWrites=true&w=majority&appName=Cluster0', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -6,4 +6,3 @@ export const fetchTasks = () => API.get('/tasks');
 export const createTask = (task) => API.post('/tasks', task);
 export const toggleTask = (id) => API.put(`/tasks/${id}`);
 export const deleteTask = (id) => API.delete(`/tasks/${id}`);
-
