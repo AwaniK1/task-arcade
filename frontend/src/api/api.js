09fbdb392033f02053a198e317534dc3d8905363
Awani K <awani.18159@sakec.ac.in>
2025-10-16 13:41:33 +0530

    updated api for api backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://task-arcade-backend.onrender.com/api' });
 
 export const fetchTasks = () => API.get('/tasks');
 export const createTask = (task) => API.post('/tasks', task);
