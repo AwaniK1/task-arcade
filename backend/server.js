8ffba5f927e89d276776e35df8169e5a77ae222e
Awani K <awani.18159@sakec.ac.in>
2025-10-16 12:42:46 +0530

    Backend setup 2

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const taskRoutes = require('./routes/taskRoutes');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/api/tasks', taskRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Dynamic PORT for Render
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
